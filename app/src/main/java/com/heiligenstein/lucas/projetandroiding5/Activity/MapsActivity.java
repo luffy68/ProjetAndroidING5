28aa02961f727236169b39da2ff9bf18c96962cd
InfinitySoul <privateadrienps@gmail.com>
2018-12-09 21:52:44 +0100

    Affichage de la position sur la carte

--- app/src/main/java/com/heiligenstein/lucas/projetandroiding5/Activity/MapsActivity.java
+++ app/src/main/java/com/heiligenstein/lucas/projetandroiding5/Activity/MapsActivity.java
@@ -2,6 +2,7 @@ package com.heiligenstein.lucas.projetandroiding5.Activity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -47,6 +48,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -57,5 +59,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return;
         }
         mMap.setMyLocationEnabled(true);
+    }
+
+    public void onLocationChanged(Location location){
+        LatLng me = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(me).title("Marker me"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(me));
+        /*mMap.setMaxZoomPreference(mMap.getMaxZoomLevel());*/
     }
 }
